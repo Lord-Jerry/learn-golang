5083ed42ad7656cf176aca289d51717e06967f26
Lord Jerry <lovinjerry004@gmail.com>
2020-08-03 22:24:29 +0100

    variables 😇

---
+++ var.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = "This is a damn string"
+	a = "hey"
+	fmt.Println(a)
+
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+
+	d := true
+	fmt.Println(d)
+
+	var e bool
+	fmt.Println(e)
+}
